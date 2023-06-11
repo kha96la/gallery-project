d2c20459fb066b7cdfa3e154e3eb826c57e1c4ab
kha96la <khawla.hmr96@gmail.com>
2023-06-11 02:43:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+const img4 = document.getElementById('img4');
+const main = document.getElementById('main');
+const img13 = document.getElementById('img5');
+const img14 = document.getElementById('img6');
+const img15 = document.getElementById('img7');
+const img16 = document.getElementById('img8');
+
+
+img1.addEventListener('click', function() {
+    main.src = this.src;
+    
+
+   
+     
+
+});
+
+img2.addEventListener('click', function() {
+    main.src = this.src;
+
+});
+
+img3.addEventListener('click', function() {
+    main.src = this.src;
+
+});
+
+img4.addEventListener('click', function() {
+    main.src = this.src;
+    
+
+});
+
+img5.addEventListener('mouseenter', function() {
+    main.src = this.src;
+
+});
+
+img6.addEventListener('mouseenter', function() {
+    main.src = this.src;
+
+});
+
+img7.addEventListener('mouseenter', function() {
+    main.src = this.src;
+
+});
+
+img8.addEventListener('mouseenter', function() {
+    main.src = this.src;
+
+});
+
+      
+
+
+
